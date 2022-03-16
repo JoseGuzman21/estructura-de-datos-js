48fb009d1ce81c3a2e8574a744623c06fc631c77
uniondeveloper2021 <86847362+uniondeveloper2021@users.noreply.github.com>
2022-03-16 16:57:42 -0500

    add map.js

--- map/map.js
+++ map/map.js
@@ -1,7 +1,35 @@
-let numbers = [1, 5, 10, 15];
+function methods() {
+    let numbers = [
+        // {
+        //     sku: '1',
+        //     status: true
+        // },
+        // {
+        //     sku: '2',
+        //     status: false
+        // },
+        // {
+        //     sku: '3',
+        //     status: true
+        // }
+    ];
 
-let doubles = numbers.map(function (x) {
-    if (x == 2) {
-        return x * 2;
+    let filteredWithOutSku = []
+
+    let filtered = numbers.find((number) => number.sku !== '3')
+
+    if (filtered === undefined) {
+        numbers.push({ sku: '3', status: true });
     }
-});
+    if (filtered !== undefined) {
+        filteredWithOutSku = numbers.filter((number) => number.sku != '3');
+        numbers = [];
+        numbers.push(...filteredWithOutSku);
+        numbers.push({ sku: '3', status: true });
+    }
+
+    console.log(numbers);
+}
+
+methods();
+
